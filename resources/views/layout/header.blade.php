b20abc5c92d7e8ad98637ad603f78227c5efc71f
unknown <mobinaghashahi13@gmail.com>
2023-06-21 10:07:43 +0330

    create Header

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head lang="fa">
+
+    <title>وبلاگ شخصی مبین آقاشاهی</title>
+    <link rel="shortcut icon" href="/logo/logos.ico">
+
+    <meta name="title" content="وبلاگ شخصی مبین آقاشاهی">
+    <meta name="description"
+          content="یک آسیایی قضا و قدری که به کامپیوترها دستور میده!
+          علاقمند به لینوکس، Full Stack Developer، شبکه و امنیتش، خانه های هوشمند.">
+    <meta name="keywords" content="وبلاگ شخصی مبین آقاشاهی">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="fa">
+    <meta name="author" content="محمد مبین آقاشاهی اردستانی">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/RWD.css?v=3.8" rel="stylesheet">
+
+
+</head>
+<body>
+
+<header>
+    <div class="col-12 headerIcon" style="background-color: #0e0e0e;">
+        <div style="padding: 10px 0px 0px 10px">
+            <a href="https://www.instagram.com/aghashahi.mobin/?hl=en">
+                <img src="/logos/instagram.png" width="30" height="30" alt="instagram">
+            </a>
+            <a href="https://twitter.com/aghashahi_mobin">
+                <img src="/logos/twitter.png" width="30" height="30" alt="twitter">
+            </a>
+            <a href="https://github.com/mobinaghashahi">
+                <img src="/logos/github.png" width="30" height="30" alt="github">
+            </a>
+            <a href="https://t.me/mobinaghashahi">
+                <img src="/logos/telegram.png" wwidth="30" height="30" alt="telegram">
+            </a>
+            <a href="https://www.linkedin.com/in/mobin-aghashahi-4a09a0155">
+                <img src="/logos/linkedin.png" width="30" height="30" alt="linkedin">
+            </a>
+        </div>
+    </div>
+    <div class="col-12 " style="padding: 0px;margin: 0px">
+        <div style="margin: auto">
+            <a href="/">
+                <img class="logo" src="/logos/icon.png">
+            </a>
+        </div>
+    </div>
+    <div class="col-12 headerText" style="background-color: #0e0e0e;color: white;text-align: right">
+        <div style="padding: 20px">
+            <div style="text-align: right;float: right;padding-bottom: 20px">
+                <a href="/">
+                    خانه
+                </a>
+            </div>
+            <div style="text-align: right;float: right;padding-bottom: 20px;margin-right: 10px">
+                <a href="/">
+                    پروژه ها
+                </a>
+            </div>
+            <div style="text-align: right;float: right;padding-bottom: 20px;margin-right: 10px">
+                <a href="/">
+                    درباره من
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
+
