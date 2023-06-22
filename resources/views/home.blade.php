4ca59c5b8dc95026eaabbbb28585c7c0a897c6d3
unknown <mobinaghashahi13@gmail.com>
2023-06-22 09:14:58 +0330

    some change on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,12 @@
             <div class="col-5"
                  style=";margin: 20px 0px 20px 0px;padding: 10px;text-align: right;background-color: #36363BFF;border-radius: 10px">
                 <h1>جدید ترین نوشته ها</h1>
-                <div class="col-12">
+                <div class="col-12" style="padding-bottom: 20px">
                     <div style="width: 100%;float: right">
                         <h2>چگونه تحریم گذر بسازیم؟</h2>
                         <img src="/logos/test.jpg"
                              style="max-width: 100%">
-                        <p>این یک پست تست است و ارزش دیگری ندارد ش سیبسیب سیب سیبشسیب سیب سیب سیبسیتشنسمیتسشمنی
+                        <p style="text-align: justify;direction: rtl">این یک پست تست است و ارزش دیگری ندارد ش سیبسیب سیب سیبشسیب سیب سیب سیبسیتشنسمیتسشمنی
                             تمشنستمنشستی</p>
                     </div>
                     <div class="col-12" style="direction: rtl;font-size: 12px;color: #b7b7b7;">
@@ -24,12 +24,12 @@
                     </div>
                 </div>
                 <hr class="col-12" style="float: none">
-                <div class="col-12">
+                <div class="col-12" style="padding-bottom: 20px">
                     <div style="width: 100%;float: right">
                         <h2>چگونه تحریم گذر بسازیم؟</h2>
                         <img src="/logos/test.jpg"
                              style="max-width: 100%">
-                        <p>این یک پست تست است و ارزش دیگری ندارد ش سیبسیب سیب سیبشسیب سیب سیب سیبسیتشنسمیتسشمنی
+                        <p style="text-align: justify;direction: rtl">این یک پست تست است و ارزش دیگری ندارد ش سیبسیب سیب سیبشسیب سیب سیب سیبسیتشنسمیتسشمنی
                             تمشنستمنشستی</p>
                     </div>
                     <div class="col-12" style="direction: rtl;font-size: 12px;color: #b7b7b7;">
